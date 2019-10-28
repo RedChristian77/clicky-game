35cf6f219a4dd9091ad7022798338e35baf20fb0
RedChristian77 <51823396+RedChristian77@users.noreply.github.com>
2019-10-28 12:07:33 -0500

    Updates

---
+++ precache-manifest.f88747517919e6f16cb0bed5974d4432.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40da71c64d4d975188df31dd560f824f",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "fac0f8348b0a41175596",
+    "url": "/clicky-game/static/css/main.e2bfea17.chunk.css"
+  },
+  {
+    "revision": "6e0a79bfcd4048d71765",
+    "url": "/clicky-game/static/js/2.60802817.chunk.js"
+  },
+  {
+    "revision": "fac0f8348b0a41175596",
+    "url": "/clicky-game/static/js/main.177097d3.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  },
+  {
+    "revision": "73e9910613408128b0eccb9aa7aee4b9",
+    "url": "/clicky-game/static/media/Andrew.73e99106.jpg"
+  },
+  {
+    "revision": "77e3c4349d90573865d0c8b48e706a13",
+    "url": "/clicky-game/static/media/CoachSteve.77e3c434.jpg"
+  },
+  {
+    "revision": "714273dfc3b491dfc8ac10a57f0796f0",
+    "url": "/clicky-game/static/media/Connie.714273df.jpg"
+  },
+  {
+    "revision": "b9326ed30ebd0eea32f6e928172cb9d2",
+    "url": "/clicky-game/static/media/Jay.b9326ed3.jpg"
+  },
+  {
+    "revision": "944612a50ca93f1dfbe3b0438dc5e829",
+    "url": "/clicky-game/static/media/Jessi.944612a5.jpg"
+  },
+  {
+    "revision": "22f1cecff82e8b1a1d2bad186c66636a",
+    "url": "/clicky-game/static/media/Judd.22f1cecf.jpg"
+  },
+  {
+    "revision": "13e2908cba87db29ddb7ed7dfad4114f",
+    "url": "/clicky-game/static/media/Missy.13e2908c.jpg"
+  },
+  {
+    "revision": "d5abaf1a07d477043921efe76156b8c8",
+    "url": "/clicky-game/static/media/NickBirch.d5abaf1a.jpg"
+  },
+  {
+    "revision": "876620b80dc436244dda2bbd3b8fe98f",
+    "url": "/clicky-game/static/media/Ricky.876620b8.jpg"
+  },
+  {
+    "revision": "677a2b0cee1304ef85dce878f09e4f00",
+    "url": "/clicky-game/static/media/ShameWizard.677a2b0c.jpg"
+  }
+]);
